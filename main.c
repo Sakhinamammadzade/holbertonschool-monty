a5b8b51b513cb7d00a8e6766c63e902a7291872f
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-12 04:15:06 -0800

    main.c seperated execute function

--- main.c
+++ main.c
@@ -11,26 +11,46 @@ int main(int argc, char *argv[0])
 {
 if (argc != 2)
 {
-fprintf(stderr, "Usage :%s <integer>\n", argv[0]);
+fprintf(stderr, "Usage :%s <filename>\n", argv[0]);
 exit(EXIT_FAILURE);
 }
-FILE *file = fopen(argv[1], "r");
-if (!file)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE);
-}
-global_stack = NULL;
-char *line = NULL;
-size_t len = 0;
-unsigned int line_number = 0;
 
-while (getline(&line, &len, file) !=  -1)
-{
-line_number++;
-}
-free(line);
-fclose(file);
+execute_file(argv[1]);
 return (EXIT_SUCCESS);
 }
 
+/**
+ *execute_file - file execute
+ @filename: pointer
+ *Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+void execute_file(char *filename)
+{
+FILE *file = fopen(filename, "r");
+if (!file)
+{
+fprinf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
+char *line = NULL;
+size_t len = 0;
+unsigned int line_number = 0;
+while (getline(&line, &len, file) != -1)
+{
+line_number++;
+char *token = strtok(line, "$$");
+while (token != NULL)
+{
+if (strcmp(token, "push 1") == 0)
+{
+push(&global_stack, 1);
+}
+else if (strcmp(token, "push 2") == 0)
+{
+push(&global_stack, 2);
+}
+token = strtok(NULL, "$$");
+}
+}
+free(line);
+fclose(file);
+}
