08a15fbe1b19d3eaff4de5f7ddf490bef456f46a
fatimasgit <fatimamedova@inbox.ru>
2024-01-13 05:42:37 -0800

    swap was added

--- swap.c
+++ swap.c
@@ -5,12 +5,15 @@ void swap(stack_t **stk, unsigned int linenum)
 	stack_t *tmp = NULL;
 	int tmp_n = 0;
 
-	if(*stack == NULL || (*stack)->next == NULL) 
+	if(*stk == NULL || (*stk)->next == NULL) 
 	{
 		fprintf(stderr,"L%d: can't swap, stack too short\n",linenum);
 		exit(EXIT_FAILURE);
 	}
-	`
+	tmp = *stk;
+	tmp_n = tmp->n;
+	tmp->n = tmp_n;
 
-	
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
 }
