871cb5e9ffe5361028e540d62c73d17b42472bf7
fatimasgit <fatimamedova@inbox.ru>
2024-01-13 06:24:20 -0800

    corrected the error of swapping the same value

--- swap.c
+++ swap.c
@@ -1,19 +1,21 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void swap(stack_t **stk, unsigned int linenum)
 {
 	stack_t *tmp = NULL;
-	int tmp_n = 0;
 
-	if(*stk == NULL || (*stk)->next == NULL) 
+	if (*stk == NULL || (*stk)->next == NULL) 
 	{
 		fprintf(stderr,"L%d: can't swap, stack too short\n",linenum);
 		exit(EXIT_FAILURE);
 	}
-	tmp = *stk;
-	tmp_n = tmp->n;
-	tmp->n = tmp_n;
 
+	tmp = *stk;
+
+	int tmp_n = tmp->n;
 	tmp->n = tmp->next->n;
 	tmp->next->n = tmp_n;
 }
