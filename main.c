bd76e74be4957be5748365a60f4ee921f96a1f50
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-13 11:53:17 -0800

    error message chenge

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+	        dprintf(2, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buff, &s, file) != -1)
