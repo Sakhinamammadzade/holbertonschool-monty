6e776619c6f6102ea05466f9a02b0acf3208bbf0
fatimasgit <fatimamedova@inbox.ru>
2024-01-13 13:49:42 -0800

    fixed a betty error

--- main.c
+++ main.c
@@ -64,7 +64,7 @@ void _tokenizer(char *string, stack_t **stk, unsigned int linenum)
 		token = strtok(NULL, " ");
 		if (!check_digit(token))
 		{
-			dprintf(2,"L%d: usage: push integer\n", linenum);
+			dprintf(2, "L%d: usage: push integer\n", linenum);
 			free_stk(stk, linenum);
 			exit(EXIT_FAILURE);
 		}
@@ -81,8 +81,7 @@ void _tokenizer(char *string, stack_t **stk, unsigned int linenum)
  * Return: 1 if number, 0 if not
  */
 int check_digit(char *token)
-{       
-
+{
 	if (token == NULL)
 		return (0);
 
