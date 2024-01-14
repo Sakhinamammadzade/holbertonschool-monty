27edaed05cd0839da5b45c584a55c7a93fa36fdd
fatimasgit <fatimamedova@inbox.ru>
2024-01-14 08:34:09 -0800

    created add function

---
+++ add.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * add - function to add top two elements on stack
+ * @stk: stack
+ * @linenum: line number
+ * Return: void
+ */
+
+void add(stack_t **stk, unsigned int linenum)
+{
+	int adding;
+	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
+	{
+		printf("L%d: can't add, stack too short\n", linenum);
+		free_stk(stk, linenum);
+		exit(EXIT_FAILURE);
+	}
+	adding = variables.holder;
+	pop(stk, linenum);
+	adding += variables.holder;
+	pop(stk, linenum);
+	variables.holder = adding;
+	push(stk, linenum);
+}
