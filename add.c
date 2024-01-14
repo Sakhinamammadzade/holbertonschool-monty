cbffa62e40f06cb29725d69fdd505ef988d8853a
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-14 10:47:43 -0800

    add.c compiler error fixed i hope thats end

--- add.c
+++ add.c
@@ -6,26 +6,10 @@
  * @linenum: line number
  * Return: void
  */
-<<<<<<< HEAD
 void add(stack_t **stk, unsigned int linenum)
 {
 	int adding;
 
-	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
-	{
-		dprintf(2, "L%d: can't add, stack too short\n", linenum);
-		free_stk(stk, linenum);
-		exit(EXIT_FAILURE);
-	}
-        
-	adding = (*stk)->n + (*stk)->next->n;
-	pop(stk, linenum);
-	(*stk)->n = adding;
-=======
-
-void add(stack_t **stk, unsigned int linenum)
-{
-	int adding;
 	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
 	{
 		printf("L%d: can't add, stack too short\n", linenum);
@@ -38,5 +22,5 @@ void add(stack_t **stk, unsigned int linenum)
 	pop(stk, linenum);
 	variables.holder = adding;
 	push(stk, linenum);
->>>>>>> 6f191493e420606f74bbc8abc63ddbbac4bc6ab8
+
 }
