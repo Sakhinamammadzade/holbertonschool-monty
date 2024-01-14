ca1bab91015bdef1a70ea506cc93c7b469b7cc5d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-14 10:30:00 -0800

    modified add

---
+++ add.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * add - function to add top two elements on stack
+ * @stk: stack
+ * @linenum: line number
+ * Return: void
+ */
+void add(stack_t **stk, unsigned int linenum)
+{
+	int adding;
+
+	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
+	{
+		dprintf(2, "L%d: can't add, stack too short\n", linenum);
+		free_stk(stk, linenum);
+		exit(EXIT_FAILURE);
+	}
+        
+	adding = (*stk)->n + (*stk)->next->n;
+	pop(stk, linenum);
+	(*stk)->n = adding;
+}
